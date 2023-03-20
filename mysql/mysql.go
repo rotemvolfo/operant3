8e602905921e7280daa63074b53271627a58648b
rotemvolfo <38879084+rotemvolfo@users.noreply.github.com>
2023-03-19 23:41:34 -0700

    Update mysql.go

--- mysql/mysql.go
+++ mysql/mysql.go
@@ -15,7 +15,7 @@ import (
 var Db *sql.DB
 
 func InitDB() {
-	// Use root:dbpass@tcp(172.17.0.2)/hackernews, if you're using Windows.
+	
 	db, err := sql.Open("mysql", "root:mysql@tcp(127.0.0.1:3306)/operant")
 	if err != nil {
 		log.Panic(err)
